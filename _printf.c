b30abf886685d25f7f437352b74f0e58259acf93
celestuwa <celestuwa@gmail.com>
2022-06-05 03:18:28 -0700

    main.h

--- _printf.c
+++ _printf.c
@@ -7,17 +7,9 @@
  */
 int _printf(const char *format, ...);
 {
-convert_match m[] = {
-{"%s", printf_string}, {"%c", printf_char},
-{"%%", printf_37},
-{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-};
-
 va_list args;
-int i = 0, j, len = 0;
+int i = 0,
+int n_printed = 0;
 
 va_start(args, format);
 if (format == NULL || (format[0] == '%' && format[1] == '\0'))
